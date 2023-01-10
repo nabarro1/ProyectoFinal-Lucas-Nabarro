474a745ba3be4f4e7217f2adc7b301ee6b696500
Lucas Nabarro <lucas.f.nabarro@hotmail.com>
2023-01-10 17:23:37 -0300

    first commit

---
+++ AppCoder/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(Animal)
+admin.site.register(Persona)
+admin.site.register(Veterinario)
+admin.site.register(Avatar)
